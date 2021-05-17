6407be85fb540aed959e194b91cd10ff44a2104c
FatimaDev <yaya.fatima.ezzahra@gmail.com>
2021-05-17 18:26:27 +0200

    pizza website

---
+++ src/components/Products/data.js
@@ -0,0 +1,99 @@
+import product1 from '../../images/pizza.jpeg'
+import product2 from '../../images/pizza2.jpeg'
+import product3 from '../../images/pizza3.jpeg'
+import sweet1 from '../../images/donuts.jpeg'
+import sweet2 from '../../images/dessert8.jpeg'
+import sweet3 from '../../images/dessert5.jpeg'
+import juice1 from '../../images/juiceo.jpeg'
+import juice2 from '../../images/juices.jpeg'
+import juice3 from '../../images/juicec.jpeg'
+
+
+
+export const productData = [
+    {
+     img: product1,
+     alt:'Pizza',
+     name:'Cheese Pizza',
+     desc:'Tomato sauce, mozzarella, and oregano.',
+     price:'$19.99',
+     button:'Add to Cart'
+     
+    },
+    {
+        img: product2,
+        alt:'Pizza',
+        name:' Pepperoni Pizza',
+        desc:' Tomato sauce, mozzarella, mushrooms, ham, artichokes, olives',
+        price:'$30.00',
+        button:'Add to Cart'
+        
+       },
+       {
+        img: product3,
+        alt:'Pizza',
+        name:' Emiliana Pizza',
+        desc:' Mozzarella, spinach, ricotta cheese, and parmesan',
+        price:'$25.99',
+        button:'Add to Cart'
+        
+       },
+];
+export const productDataTwo = [
+    {
+     img: sweet1,
+     alt:'Donuts',
+     name:'Doughlicious',
+     desc:'Belgian chocolate glazed donuts covered in icing with sprinkles on top',
+     price:'$35.99',
+     button:'Add to Cart'
+     
+    },
+    {
+        img: sweet2,
+        alt:'Ice Cream',
+        name:' Caramel Wonder',
+        desc:'Vanilla ice cream covered with caramel and chocolate glaze topped with a coco stick',
+        price:'$40.00',
+        button:'Add to Cart'
+        
+       },
+       {
+        img: sweet3,
+        alt:'Brownie ',
+        name:' Brownie Bunch ',
+        desc:' Double fudge brownie squares topped with white chocolate pieces and macadamia nuts',
+        price:'$25.99',
+        button:'Add to Cart'
+        
+       },
+];
+export const productDataTree = [
+    {
+     img: juice1,
+     alt:'juice',
+     name:'Orange ',
+     desc:'tomato, green apple and carrots ',
+     price:'$20.99',
+     button:'Add to Cart'
+     
+    },
+    {
+        img: juice2,
+        alt:'juice',
+        name:'Spinach-Apple ',
+        desc:'Vanilla ice cream covered with carame',
+        price:'$19.00',
+        button:'Add to Cart'
+        
+       },
+       {
+        img: juice3,
+        alt:'juice ',
+        name:'Cucumber Juice    ',
+        desc:'fresh cucumber flavor. Coconut water and apple .',
+        price:'$35.99',
+        button:'Add to Cart'
+        
+       },
+];
